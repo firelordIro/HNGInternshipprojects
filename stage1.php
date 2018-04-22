477a85cdb5b574b95cd87c19578d0e6b672e146d
firelordIro <38477860+firelordIro@users.noreply.github.com>
2018-04-22 21:35:12 +0100

    Add files via upload

---
+++ stage1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Stage 1 Task</title>
+    </head>
+	<style>
+	body {
+	background-color:#2F80ED;	
+	}
+	
+	h1{
+		text-align: center;
+		color: #F2F2F2;
+		font-size: 40px;
+		font-family: Roboto;
+
+	}
+	
+	h3{
+		font-family: Roboto;
+		color: #F2F2F2;
+		margin-top: 300px;
+		text-align: center;
+		
+	}
+	.button1 {
+		
+		background-color: #F2F2F2;
+		color: #2F80ED;
+		font-family: Roboto;
+		font-size: 18px;
+		width: 75px;
+		height: 50px;
+		text-align: center;
+		border-style: none;
+		margin-left: 450px;
+		margin-top: 150px;
+	}
+	.button2 {
+		
+		background-color: #F2F2F2;
+		color: #2F80ED;
+		font-family: Roboto;
+		font-size: 18px;
+		width: 75px;
+		height: 50px;
+		text-align: center;
+		border-style: none;
+		margin-left: 250px;
+		margin-top: 150px;
+	}
+	
+	</style>
+    <body> 
+	
+	<h1>Welcome to HNGInternship 4.0</h1>
+	
+	
+	<input type="button" value="Login" class="button1">&nbsp;
+	
+	<input type="button" value="Sign Up" class="button2">
+	
+	
+	<?php
+        echo "<h3>".date('h:i A, jS F Y.')."</h3>";         
+    ?>
+    </body>
+</html>
